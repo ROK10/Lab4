68d1933efccf9a626afd5fc043805be0e6d7be8e
ROK10 <rok10wot@ukr.net>
2021-03-03 02:36:40 +0200

    Update api.js

--- Backend/api.js
+++ Backend/api.js
@@ -1,10 +1,7 @@
-/**
- * Created by chaika on 09.02.16.
- */
 var Pizza_List = require('./data/Pizza_List');
 
 exports.getPizzaList = function(req, res) {
-    res.send(Pizza_List);
+    res.send(Pizza_List); //Відправити назад все, що міститься в Pizza_List
 };
 
 exports.createOrder = function(req, res) {
@@ -12,6 +9,11 @@ exports.createOrder = function(req, res) {
     console.log("Creating Order", order_info);
 
     res.send({
-        success: true
+        success: true,
+        name: order_info.name,
+        phone: order_info.phone,
+        address: order_info.address,
+        pizzas: order_info.order.length,
+        price: order_info.price
     });
 };
